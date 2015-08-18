b6c3f0056862ff98b25d194df872ae0fabf7ecf4
Jess A Boerner <jessboerner@gmail.com>
2015-08-18 09:36:01 -0600

    controller updates

--- controllers/nk2.js
+++ controllers/nk2.js
@@ -4,7 +4,25 @@ function NK2() {}
 //############################################################################
 NK2.tempTimer = 0;
 NK2.XforLoop = 8;
+NK2.FXMatrix = {'flanger':{'flanger':0, 'echo':1, 'autopan':2, 'reverb':3, 'phaser':4, 'filter':9, 'moog':10, 'bitcrusher':11},
+		'echo':{'flanger':11, 'echo':0, 'autopan':1, 'reverb':2, 'phaser':3, 'filter':8, 'moog':9, 'bitcrusher':10},
+		'autopan':{'flanger':10, 'echo':11, 'autopan':0, 'reverb':1, 'phaser':2, 'filter':7, 'moog':8, 'bitcrusher':9},
+		'reverb':{'flanger':9, 'echo':10, 'autopan':11, 'reverb':0, 'phaser':1, 'filter':6, 'moog':7, 'bitcrusher':8},
+		'phaser':{'flanger':8, 'echo':9, 'autopan':10, 'reverb':11, 'phaser':0, 'filter':5, 'moog':6, 'bitcrusher':7},
+		'filter':{'flanger':3, 'echo':4, 'autopan':5, 'reverb':6, 'phaser':7, 'filter':0, 'moog':1, 'bitcrusher':2},
+		'moog':{'flanger':2, 'echo':3, 'autopan':4, 'reverb':5, 'phaser':6, 'filter':11, 'moog':0, 'bitcrusher':1},
+		'bitcrusher':{'flanger':1, 'echo':2, 'autopan':3, 'reverb':4, 'phaser':5, 'filter':10, 'moog':11, 'bitcrusher':0}
+	      }
+NK2.FX1 = ['moog', 'filter', 'autopan', 'phaser', 'echo', 'reverb']
+NK2.FX2 = [['echo', 'bitcrusher'], ['echo', 'autopan'], ['reverb', 'autopan']]
 
+NK2.fx1LED = [0x20, 0x21, 0x30, 0x31, 0x40, 0x41]
+
+NK2.effectBank = [['flanger', 'moog'], ['echo', 'bitcrusher']]
+
+
+NK2.FXOrder = ['flanger', 'echo', 'autopan', 'reverb', 'phaser', 'bessel4',
+	      'bessel8', 'lr8', 'graphiceq', 'filter', 'moog', 'bitcrusher']
 
 //############################################################################
 //defaults
@@ -29,7 +47,7 @@ NK2.leftButton={"trdown":0x3A,"trup":0x3B,"cycle":0x2E,"mset":0x3C,"mdown":0x3D,
 //############################################################################
 
 NK2.init = function init() { // called when the device is opened & set up
-	NK2.setup();
+	NK2.fxReInit()
 	
 	}
 
@@ -38,94 +56,148 @@ NK2.shutdown = function shutdown() {
 	
 	}
 
-// Buttons
-NK2.LoopRoll = function(channel, control, value, status, group) {
-    //if( NK2.ShiftStop == 0 ) {
-		if( value == 0x7F ) { //if button down only
-		    	//Channel1	Channel2
-		    if( control == 0x20 || control == 0x21 ) {
-			engine.setValue(group,"beatlooproll_0.5_activate",1);
-		    }
-		    if( control == 0x30 || control == 0x31 ) {
-			engine.setValue(group,"beatlooproll_0.25_activate",1);
-		    }
-		    if( control == 0x40 || control == 0x41 ) {
-			engine.setValue(group,"beatlooproll_0.125_activate",1);
-		    }
-		}
-		
-		else { //TODO: Logic for holding the looproll button down versus pressing it
-		    	//Channel1	Channel2
-		    if( control == 0x20 || control == 0x21 ) {
-			engine.setValue(group,"beatlooproll_0.5_activate",0);
-		    }
-		    if( control == 0x30 || control == 0x31 ) {
-			engine.setValue(group,"beatlooproll_0.25_activate",0);
-		    }
-		    if( control == 0x40 || control == 0x41 ) {
-			engine.setValue(group,"beatlooproll_0.125_activate",0);
-		    }
-		}
-}
-
-NK2.ShortLoop  = function(channel, control, value, status, group) {
-	if( value == 0x7F ) { //if button down only
-		//Channel1	Channel2
-	    if( control == 0x22 || control == 0x23 ) {
-		var enabled = engine.getValue(group, "beatloop_0.5_enabled");
-		if( enabled ) {
-		    engine.setValue(group,"beatloop_0.5",0);
-		}
-		else {
-		    engine.setValue(group,"beatloop_0.5",1);
-		}
-	    }
-	    if( control == 0x32 || control == 0x33 ) {
-		var enabled = engine.getValue(group, "beatloop_0.25_enabled");
-		if( enabled ) {
-		    engine.setValue(group,"beatloop_0.25",0);
-		}
-		else {
-		    engine.setValue(group,"beatloop_0.25",1);
-		}
-	    }
-	    if( control == 0x42 || control == 0x43 ) {
-		var enabled = engine.getValue(group, "beatloop_0.125_enabled");
-		if( enabled ) {
-		    engine.setValue(group,"beatloop_0.125",0);
-		}
-		else {
-		    engine.setValue(group,"beatloop_0.125",1);
-		}
-	    }
-	}
-	
-	else {
-	    
-	}
-}
-
-NK2.LightLED = function (control) {
+NK2.lightLED = function (control) {
 	midi.sendShortMsg(NK2.midiChannel, control, 0x7F);
 }
 
-NK2.DimLED = function(control) {
+NK2.dimLED = function(control) {
 	midi.sendShortMsg(NK2.midiChannel, control, 0x00);
 }
 
-NK2.samplerTest = function(value, group, control) {
-  if( value == 1 ) {
-    engine.setValue(group, 'start_stop', 1)
-    engine.setValue(group, 'start_stop', 0)
-    engine.setValue(group, 'play', 0)
+NK2.dimFX1 = function() {
+  for(index=0; index < 6; index++) {
+     NK2.dimLED(NK2.fx1LED[index])
   }
+}
+
+NK2.fxReInit = function() {
+  engine.setValue("[EffectRack1]", "clear", 1);
+  engine.setValue("[EffectRack1]", "clear", 0);
   
+  //LEFT EFFECTS
+  //First effect flanger
+  engine.setValue("[EffectRack1_EffectUnit1_Effect1]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit1_Effect1]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit1_Effect1]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit1_Effect1]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit1_Effect1]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit1_Effect1]", "prev_effect", 0);
   
+  //second effect filter
+  engine.setValue("[EffectRack1_EffectUnit1_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit1_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit1_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit1_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit1_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit1_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit1_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit1_Effect2]", "prev_effect", 0);
+  
+  //RIGHT EFFECTS
+  //first effect echo
+  engine.setValue("[EffectRack1_EffectUnit2_Effect1]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit2_Effect1]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit2_Effect1]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit2_Effect1]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit2_Effect1]", "prev_effect", 0);
+
+  
+  //second effect bitcrusher
+  engine.setValue("[EffectRack1_EffectUnit2_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit2_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit2_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit2_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit2_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit2_Effect2]", "prev_effect", 1);
+  engine.setValue("[EffectRack1_EffectUnit2_Effect2]", "prev_effect", 0);
+}
+
+NK2.fxChange = function (channel, control, value, status, group) {
+  if( value == 0x7F ) {
+    if( group = '[EffectRack1_EffectUnit1_Effect2]' ) { //only changing effect 2 on this rack
+      NK2.dimFX1()
+      var from = NK2.effectBank[0][1]
+      if( control == NK2.Sbutton[1] ) {//default
+	var to = NK2.FX1[0]
+	NK2.effectBank[0][1] = NK2.FX1[0]
+	NK2.lightLED(NK2.Sbutton[1])
+      }
+      else if( control == NK2.Mbutton[1] ) {//reg filter
+	var to = NK2.FX1[1]
+	NK2.effectBank[0][1] = NK2.FX1[1]
+	NK2.lightLED(NK2.Mbutton[1])
+      }
+      else if( control == NK2.Rbutton[1] ) {//reg filter
+	var to = NK2.FX1[2]
+	NK2.effectBank[0][1] = NK2.FX1[2]
+	NK2.lightLED(NK2.Rbutton[1])
+      }
+      else if( control == NK2.Sbutton[2] ) {//default
+	var to = NK2.FX1[3]
+	NK2.effectBank[0][1] = NK2.FX1[3]
+	NK2.lightLED(NK2.Sbutton[2])
+      }
+      else if( control == NK2.Mbutton[2] ) {//reg filter
+	var to = NK2.FX1[4]
+	NK2.effectBank[0][1] = NK2.FX1[4]
+	NK2.lightLED(NK2.Mbutton[2])
+      }
+      else if( control == NK2.Rbutton[2] ) {//reg filter
+	var to = NK2.FX1[5]
+	NK2.effectBank[0][1] = NK2.FX1[5]
+	NK2.lightLED(NK2.Rbutton[2])
+      }
+      var effect2Jump = NK2.FXMatrix[from][to]
+      
+      for(index=0; index < effect2Jump; index++) {
+	
+	engine.setValue(group, "next_effect", 1);
+      }
+      engine.setValue(group, "next_effect", 0);
+    }
+    
+    else { //possible to change both effects on this rack
+      var from1 = NK2.effectBank[1][0]
+      var from2 = NK2.effectBank[1][1]
+      if( control == NK2.Sbutton[3] ) {//default
+	var to1 = NK2.FX2[0][0]
+	var to2 = NK2.FX2[0][1]
+	NK2.effectBank[1] = NK2.FX2[0]
+	NK2.lightLED(NK2.Sbutton[3])
+      }
+      else if( control == NK2.Mbutton[3] ) {//reg filter
+	var to1 = NK2.FX2[1][0]
+	var to2 = NK2.FX2[1][1]
+	NK2.effectBank[1] = NK2.FX2[0]
+	NK2.lightLED(NK2.Mbutton[3])
+      }
+      else if( control == NK2.Rbutton[3] ) {//reg filter
+	var to1 = NK2.FX2[2][0]
+	var to2 = NK2.FX2[2][1]
+	NK2.effectBank[1] = NK2.FX2[0]
+	NK2.lightLED(NK2.Rbutton[3])
+      }
+      var effect3Jump = NK2.FXMatrix[from1][to1]
+      var effect4Jump = NK2.FXMatrix[from2][to2]
+      
+      for(index=0; index < effect3Jump; index++) {
+	engine.setValue(group, "next_effect", 1);
+      }
+      
+      engine.setValue(group, "next_effect", 0);
+      
+      for(index=0; index < effect4Jump; index++) {
+	engine.setValue("[EffectRack1_EffectUnit1_Effect4]", "next_effect", 1);
+      }
+      engine.setValue("[EffectRack1_EffectUnit1_Effect4]", "next_effect", 0);
+      
+    }
+  }
 }
 
 NK2.setup = function(obj) {
-  //setup trigger for sampler1 end playback
-  engine.connectControl('[Sampler1]', 'playposition', 'NK2.samplerTest')
+  
+
 
 }
 
